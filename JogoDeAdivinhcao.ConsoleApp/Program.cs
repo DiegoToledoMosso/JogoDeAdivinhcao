c0749a5c91fca84165f14c29ab68746ceb873432
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-03-25 18:24:06 -0300

    Versão 1: Estrutura básica e entrada do usuário Versão 2: Gerar um número secreto aleatório

--- JogoDeAdivinhcao.ConsoleApp/Program.cs
+++ JogoDeAdivinhcao.ConsoleApp/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace JogoDeAdivinhcao.ConsoleApp
+﻿using System;
+
+namespace JogoDeAdivinhcao.ConsoleApp
 {
     internal class Program
     {
+        //  Versão 1: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Jogo de Adivinhção");
+            Console.WriteLine("--------------------------------------------");
+
+            //Versão 2: Gerar um número secreto aleatório
+            // Lógica do jogo
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            Console.Write("Digite um número (de 1 à 20) para chutar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você digitou o número: " + numeroDigitado);
+            Console.WriteLine("O número secreto é: " + numeroSecreto);
+
+
+            Console.ReadLine();
+
         }
     }
 }
